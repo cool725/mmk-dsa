01d3a25d3c9ae92aec7fbb89cb3607ee1522ad31
cool725 <109250241+cool725@users.noreply.github.com>
2021-04-13 04:38:37 +0000

    Merged in dsa-welcome-page (pull request #54)
    Dsa welcome page
    
    * LAP-188 Updating welcome page UI
    
    * LAP-188 removing redundant else if condition
    
    * LAP-188 Allowing agent to update the application in case of 'info_required' status
    
    Approved-by: Anton Karpenko

--- src/views/Welcome/Welcome.tsx
+++ src/views/Welcome/Welcome.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Grid, LinearProgress, Typography } from '@material-ui/core';
+import { Grid, LinearProgress, Typography, Card, CardHeader, CardContent } from '@material-ui/core';
 import api from '../../api';
 import { useAppStore } from '../../store';
+import { useFormStyles } from '../styles';
 
 /**
  * Renders "Welcome" view
@@ -12,7 +13,7 @@ const WelcomeView = () => {
   const history = useHistory();
   const [state] = useAppStore();
   const [loading, setLoading] = useState(true);
-
+  const classes = useFormStyles();
   const [dsaId, setDsaId] = useState<string>();
   const [dsaStatus, setStatus] = useState<string>();
 
@@ -26,8 +27,10 @@ const WelcomeView = () => {
       const apiData = await api.dsa.read('', { filter: { email: email }, single: true });
       if (!componentMounted) return; // Component was unmounted while we are calling the API, do nothing!
 
-      if (!apiData || !apiData?.id) {
+      if (!apiData || !apiData?.id || apiData?.application_status === 'info_required') {
         // There is no DSA application for current user, go to Step1
+        // or if analyst marks the application as "info_required"
+        // take the user to Step 1 of application
         // console.log('Going to DSA Step 1')
         history.push('/dsa/1');
         return;
@@ -57,15 +60,24 @@ const WelcomeView = () => {
 
   const { currentUser: user } = state;
   const fullName = [user?.first_name || '', user?.last_name || ''].join(' ').trim();
+  let cardTitle = '';
+
+  if(dsaStatus === 'approved') {
+    cardTitle = `Congratulations ${fullName}!`;
+  } else {
+    cardTitle = `Welcome ${fullName}!`;
+  }
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h6">Welcome {fullName}</Typography>
-
-        <Typography variant="body1">
-          Your application (id: <b>{dsaId}</b>) has status: <b>{dsaStatus}</b>
-        </Typography>
+    <Grid container direction="column" alignItems="center">
+      <Grid item className={classes.formBody}>
+        <Card>
+          <CardHeader title={cardTitle} />
+          <CardContent>
+            {dsaStatus !== 'approved' && <Typography variant="body1">Your application is successfully submitted and is under review</Typography>}
+            {dsaStatus === 'approved' && <Typography variant="body1">Your application with ID <b>{dsaId}</b> has been <b>Approved</b></Typography>}
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );
